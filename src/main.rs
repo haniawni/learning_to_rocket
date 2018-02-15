89c99b9bb8097b3fe153a382e7a48efa7f3efde2
Hani Awni <hani.awni@gmail.com>
2018-02-15 18:08:24 -0500

    use templates to serve site

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,38 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
+
+use rocket_contrib::Template;
+use rocket::response::Redirect;
+
+// #[get("/")]
+// fn index() -> &'static str {
+//     "Hello, world!"
+// }
+#[derive(Serialize)]
+struct Context {
+	food: String,
+}
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Redirect {
+	Redirect::to("/BAGEL")
+}
+
+#[get("/eat/<food>")]
+fn eat(food: String) -> Template {
+	let context = Context{
+		food: food
+	};
+
+	Template::render("index",&context)
 }
 
 fn main() {
     rocket::ignite()
-    .mount("/", routes![index])
+    .mount("/", routes![index, eat])
+    .attach(Template::fairing())
     .launch();
 } 
